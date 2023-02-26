419c6b12c2f26d969b673f5e563948ca0a888829
Bruno Henrique de Souza <bhenriq.souza@gmail.com>
2023-02-26 15:31:19 -0300

    Adding the route table and its associations

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "free_camp_vpc" {
-  cidr_block            = "10.123.0.0/16"
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     "Name" = "free_camp"
@@ -17,4 +17,31 @@ resource "aws_subnet" "free_camp_subnet" {
   tags = {
     "Name" = "free_camp_public_subnet"
   }
+}
+
+resource "aws_internet_gateway" "free_camp_internet_gw" {
+  vpc_id = aws_vpc.free_camp_vpc.id
+
+  tags = {
+    "Name" = "free_camp_internet_gw"
+  }
+}
+
+resource "aws_route_table" "free_camp_public_rt" {
+  vpc_id = aws_vpc.free_camp_vpc.id
+
+  tags = {
+    "Name" = "free_camp_public_rt"
+  }
+}
+
+resource "aws_route" "free_camp_default_route" {
+  route_table_id         = aws_route_table.free_camp_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.free_camp_internet_gw.id
+}
+
+resource "aws_route_table_association" "free_camp_public_rt_association" {
+  subnet_id      = aws_subnet.free_camp_subnet.id
+  route_table_id = aws_route_table.free_camp_public_rt.id
 }
